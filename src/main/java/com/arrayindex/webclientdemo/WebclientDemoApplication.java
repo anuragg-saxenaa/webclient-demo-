a4d497345b9a92f010ff18e53ea8c1e79799fbd1
Anurag Saxena <anuragg.saxenaa@gmail.com>
2020-09-25 02:25:52 -0400

    initial commit

---
+++ src/main/java/com/arrayindex/webclientdemo/WebclientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.arrayindex.webclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arrayindex/webclientdemo/WebclientDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrayindex.webclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
